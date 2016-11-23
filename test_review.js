5fd2cc59a03928fcaa5428bf8789abac8aa61d40
zedricka <zedrick.ara@gmail.com>
2016-11-23 09:10:34 -1000

    wow

---
+++ test_review.js
@@ -0,0 +1,44 @@
+/* 
+Zedrick Ara 
+11/23/2016
+Javascript Test Review
+*/
+
+var knight = "Arthur";
+var maiden = "Guinevere";
+var monster = "Dragon";
+
+var weaponChest = ("Mace", "Sorcerer's Stone", "Excalibur");
+var wizard = "Merlin";
+var weapon = weaponChest[2];
+var power = 50;
+
+function swordInTheStone (worthy){
+  if ("Arthur" == worthy);
+    console.log ("We have found our King " + worthy);
+}
+  if ("Gwain" == worthy || "Lacelot" == worthy);
+    console.log ("We must keep searching, for " + worthy + " is unworthy");
+  }
+swordInTheStone(knight);
+
+function trueLove (king, queen){
+  if (queen == "Guinevere" && king == "Arthur");
+    console.log ("On this day, our " + king + " and " + queen + " shall rule happily ever after");
+}
+  if (queen == "Jane" || queen == "Morgana" || king == "Lacelot" || king == "Gwain");
+    console.log ("We must keep searching, for true love must reign over Camelot.");  
+}
+trueLove (knight, maiden);
+
+function epicBattle (wizard,enemy,power){
+  if (power <= 15);
+    console.log ("Our " + wizard + " has fallen");
+}
+  else if (power >= 16 && power <= 40){
+    console.log (wizard + " has almost slain " + enemy);
+}
+  }else{
+    console.log (wizard + " has slain the " + enemy + "!");
+}
+epicBattle (wizard, monster, power);
